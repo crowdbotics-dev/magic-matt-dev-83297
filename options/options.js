0c5201b9157494a6806f07f1044034992910cb21
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 04:43:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "magic-matt-dev-83297",
+  url: "https://magic-matt-dev-83297.botics.co",
+  api: "https://magic-matt-dev-83297.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
